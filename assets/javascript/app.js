9d1e867207aa9ba5974ed4a61e66538a9da376fa
Alex Trilling <atrill90@gmail.com>
2018-04-18 08:58:07 -0400

    final touches

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,7 +23,7 @@ function getLocation(callback) {
 }
 
 getLocation(function (data) {
-    let url = `https://app.ticketmaster.com/discovery/v2/events.json?size=10&startDateTime=${dateSubString}&latlong=${data}&radius=30&unit=miles&apikey=nEMd0Ed2sNkvX2uizZwdCDIiuArIDwnT`;
+    let url = `https://cors-anywhere.herokuapp.com/app.ticketmaster.com/discovery/v2/events.json?size=10&startDateTime=${dateSubString}&latlong=${data}&radius=30&unit=miles&apikey=nEMd0Ed2sNkvX2uizZwdCDIiuArIDwnT`;
     //cors-anywhere.herokuapp.com/
     $.ajax({
         type: "GET",
@@ -35,7 +35,7 @@ getLocation(function (data) {
             let eventLocations = json._embedded.events;
             mapMarkerMaker(eventLocations, data);
 
-            eventList(eventLocations,data);
+            eventList(eventLocations, data);
         },
         error: function (xhr, status, err) {
             // This time, we do not end up here!
@@ -46,59 +46,59 @@ getLocation(function (data) {
 // Added this function eventList to separate it from the getLocation function, but may scrap this layout
 // to simply add another identical AJAX call to dynamically create a list of events
 // and append it to the new table on the html
-function eventList(eventLocations,data) {
+function eventList(eventLocations, data) {
     console.log(eventLocations);
-        for (var i = 0; i < eventLocations.length; i++) {
-            let eventName = JSON.stringify(eventLocations[i].name);
-            // console.log(eventName);
-            let eventImage = eventLocations[i].images[i].url;
-            //  console.log(eventImage);
-            let eventVenue = eventLocations[i]._embedded.venues[0].name;
-            // console.log(eventVenue);
-            let eventDate = new Date(eventLocations[i].dates.start.dateTime);
-            // console.log(eventDate);
-            let eventLink = eventLocations[i].url;
-            // console.log("This is the event url: " + eventLink);
-            let eventInfo = eventLocations[i].info;
-            // console.log("Event info " + eventInfo);
-           
-    
-            
+    for (var i = 0; i < eventLocations.length; i++) {
+        let eventName = JSON.stringify(eventLocations[i].name);
+        // console.log(eventName);
+        let eventImage = eventLocations[i].images[i].url;
+        //  console.log(eventImage);
+        let eventVenue = eventLocations[i]._embedded.venues[0].name;
+        // console.log(eventVenue);
+        let eventDate = new Date(eventLocations[i].dates.start.dateTime);
+        // console.log(eventDate);
+        let eventLink = eventLocations[i].url;
+        // console.log("This is the event url: " + eventLink);
+        let eventInfo = eventLocations[i].info;
+        // console.log("Event info " + eventInfo);
 
-            let eventRow = $("<tr>");           
-            eventRow.addClass("newEventRow");
-            let eventN = $("<td nameData>");
-            eventN.text(eventName);
-            let eventImg = $("<img img-fluid >");
-            eventImg.attr("src", eventImage);
-            eventImg.addClass("imageData");
-            // console.log("Event img" + eventImg);
-            let eventI = $("<td imageHolder>");
-            // console.log("This is img url"+ eventI);
-            eventI.append(eventImg);
-            let eventD = $("<td dateData>");
-            eventD.text(eventDate);
-            let eventV = $("<td venueData>");
-            eventV.text(eventVenue);
-            let saveEventButton = $("<button>");
-            saveEventButton.addClass("btn btn-info btn-md");
-            saveEventButton.addClass("tickEven");
-            saveEventButton.attr("href",eventLink);
-            saveEventButton.text("Grab Me A Seat");
-            let eventB = $("<td eventButton>");
-            eventB.append(saveEventButton);
-            
-          
-            
-            
-            eventRow.append(eventI, eventB, eventN, eventV, eventD);
-            $(".eventRow").append(eventRow);
-           
-            
-            
 
-        }
-    
+
+
+        let eventRow = $("<tr>");
+        eventRow.addClass("newEventRow");
+        let eventN = $("<td nameData>");
+        eventN.text(eventName);
+        let eventImg = $("<img img-fluid >");
+        eventImg.attr("src", eventImage);
+        eventImg.addClass("imageData");
+        // console.log("Event img" + eventImg);
+        let eventI = $("<td imageHolder>");
+        // console.log("This is img url"+ eventI);
+        eventI.append(eventImg);
+        let eventD = $("<td dateData>");
+        eventD.text(eventDate);
+        let eventV = $("<td venueData>");
+        eventV.text(eventVenue);
+        let saveEventButton = $("<button>");
+        saveEventButton.addClass("btn btn-info btn-md");
+        saveEventButton.addClass("tickEven");
+        saveEventButton.attr("href", eventLink);
+        saveEventButton.text("Grab Me A Seat");
+        let eventB = $("<td eventButton>");
+        eventB.append(saveEventButton);
+
+
+
+
+        eventRow.append(eventI, eventB, eventN, eventV, eventD);
+        $(".eventRow").append(eventRow);
+
+
+
+
+    }
+
 
 }
 
@@ -113,7 +113,7 @@ function mapMarkerMaker(eventLocations, data) {
             lng: parseFloat(coordinate.longitude)
         }
     });
- 
+
     data = data.split(",");
 
     let currentLocation = {
@@ -141,17 +141,17 @@ function mapMarkerMaker(eventLocations, data) {
         var infowindow = new google.maps.InfoWindow({
             content: contentString
         });
-        locationCoordinates.forEach(function(coordinate){
+        locationCoordinates.forEach(function (coordinate) {
             console.log(coordinate.lat);
-            
-                var marker = new google.maps.Marker({
-                    position: coordinate,
-                    map: map,
-                    title: eventName
-                });
-                marker.addListener('click', function () {
-                    infowindow.open(map, marker);
-                });   
+
+            var marker = new google.maps.Marker({
+                position: coordinate,
+                map: map,
+                title: eventName
+            });
+            marker.addListener('click', function () {
+                infowindow.open(map, marker);
+            });
         });
     });
 
@@ -185,12 +185,12 @@ function mapMarkerMaker(eventLocations, data) {
 
 
 }
-$(document).on("click",'.tickEven', function(event){
+$(document).on("click", '.tickEven', function (event) {
     $(".aboutContainer").empty();
     event.preventDefault();
     window.open($(this).attr("href"), "popupWindow", "width=600,height=600,scrollbars=yes");
-     let imageGrabber = $(this).parents('.newEventRow').find('img').clone('img');
-     imageGrabber.addClass("clonedImage");
+    let imageGrabber = $(this).parents('.newEventRow').find('img').clone('img');
+    imageGrabber.addClass("clonedImage");
     imageGrabber.appendTo(".aboutContainer");
 
 
